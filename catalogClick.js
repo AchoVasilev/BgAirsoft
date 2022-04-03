70ec1315bc8a62f9894ecdf756a71522d39667f7
AngelVasilev <defiend@abv.bg>
2022-04-03 08:00:37 +0300

    Work on views

--- catalogClick.js
+++ catalogClick.js
@@ -1,40 +1,29 @@
-const btn = document.getElementById('catalog-btn');
-btn.addEventListener('click', clickHandler);
-let catalog = document.getElementById('nav-catalog');
-
-function clickHandler() {
-    btn.addEventListener('click', clickHandler);
-    if (catalog.style.display == 'none') {
-        catalog.style.display = 'block';
-    } else {
-        catalog.style.display = 'none';
-    }
-
-    const icon = document.querySelector('#catalog-btn > i');
-
-    if (icon.classList.contains('fa-bars')) {
-        icon.classList.remove('fa-bars');
-        icon.classList.add('fa-xmark');
-    } else if (icon.classList.contains('fa-xmark')) {
-        icon.classList.remove('fa-xmark');
-        icon.classList.add('fa-bars');
-    }
-}
-
-document.addEventListener('mouseup', e => documentClickHandler(e));
+document.addEventListener('click', e => documentClickHandler(e));
 
 function documentClickHandler(e) {
-    e.preventDefault();
-    if (catalog.contains(e.target)) {
-        return;
-    }
-    
-    catalog.style.display = 'none';
-    
+    let catalog = document.getElementById('nav-catalog');
     const icon = document.querySelector('#catalog-btn > i');
-    if (icon.classList.contains('fa-xmark')) {
-        icon.classList.remove('fa-xmark');
-        icon.classList.add('fa-bars');
+    if (e.target.id == 'catalog-btn' || e.target.id == 'catalog-btn-icon') {
+        if (catalog.style.display == '' || catalog.style.display == 'none') {
+            catalog.style.display = 'block';
+        } else {
+            catalog.style.display = 'none';
+        }
+
+        if (icon.classList.contains('fa-bars')) {
+            icon.classList.remove('fa-bars');
+            icon.classList.add('fa-xmark');
+        } else if (icon.classList.contains('fa-xmark')) {
+            icon.classList.remove('fa-xmark');
+            icon.classList.add('fa-bars');
+        }
     }
-    return;
+    else {
+        catalog.style.display = 'none';
+
+        if (icon.classList.contains('fa-xmark')) {
+            icon.classList.remove('fa-xmark');
+            icon.classList.add('fa-bars');
+        }
+    }
 }
--- site.js
+++ site.js
@@ -52,24 +52,32 @@ function autoShowSlides() {
     setTimeout(autoShowSlides, 4000); // Change image every 2 seconds
 }
 
-const btn = document.getElementById('catalog-btn');
-btn.addEventListener('click', clickHandler);
+document.addEventListener('click', e => documentClickHandler(e));
 
-function clickHandler() {
+function documentClickHandler(e) {
     let catalog = document.getElementById('nav-catalog');
-    if (catalog.style.display == '') {
-        catalog.style.display = 'block';
-    } else {
-        catalog.style.display = '';
-    }
-
     const icon = document.querySelector('#catalog-btn > i');
+    if (e.target.id == 'catalog-btn' || e.target.id == 'catalog-btn-icon') {
+        if (catalog.style.display == '' || catalog.style.display == 'none') {
+            catalog.style.display = 'block';
+        } else {
+            catalog.style.display = 'none';
+        }
 
-    if (icon.classList.contains('fa-bars')) {
-        icon.classList.remove('fa-bars');
-        icon.classList.add('fa-xmark');
-    } else if (icon.classList.contains('fa-xmark')) {
-        icon.classList.remove('fa-xmark');
-        icon.classList.add('fa-bars');
+        if (icon.classList.contains('fa-bars')) {
+            icon.classList.remove('fa-bars');
+            icon.classList.add('fa-xmark');
+        } else if (icon.classList.contains('fa-xmark')) {
+            icon.classList.remove('fa-xmark');
+            icon.classList.add('fa-bars');
+        }
+    }
+    else {
+        catalog.style.display = 'none';
+
+        if (icon.classList.contains('fa-xmark')) {
+            icon.classList.remove('fa-xmark');
+            icon.classList.add('fa-bars');
+        }
     }
 }
